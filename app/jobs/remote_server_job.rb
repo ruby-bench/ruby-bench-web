8f8f58f10fea7962f433c56b2a8914644ac99075
Guo Xiang Tan <tgx_world@hotmail.com>
2014-12-04 12:08:28 +0800

    RemoteServerJob should only execute in production.

--- app/jobs/remote_server_job.rb
+++ app/jobs/remote_server_job.rb
@@ -2,22 +2,24 @@ class RemoteServerJob < ApplicationJob
   queue_as :default
 
   def perform(commit_hash)
-    secrets = Rails.application.secrets
+    production? do
+      secrets = Rails.application.secrets
 
-    Net::SSH.start(
-      secrets.bare_metal_server_ip,
-      secrets.bare_metal_server_user,
-      password: secrets.bare_metal_server_password
-    ) do |ssh|
+      Net::SSH.start(
+        secrets.bare_metal_server_ip,
+        secrets.bare_metal_server_user,
+        password: secrets.bare_metal_server_password
+      ) do |ssh|
 
-      ssh.exec!("sudo docker pull tgxworld/rails_bench &&
-        sudo docker run --rm -e
-        \"RAILS_COMMIT_HASH=#{commit_hash}\" -e
-        \"RUBY_VERSION=2.1.5\" -e \"KO1TEST_SEED_CNT=100\"
-        tgxworld/rails_bench".squish
-      ) do |channel, stream, data|
+        ssh.exec!("sudo docker pull tgxworld/rails_bench &&
+          sudo docker run --rm -e
+          \"RAILS_COMMIT_HASH=#{commit_hash}\" -e
+          \"RUBY_VERSION=2.1.5\" -e \"KO1TEST_SEED_CNT=100\"
+          tgxworld/rails_bench".squish
+        ) do |channel, stream, data|
 
-        puts data
+          puts data
+        end
       end
     end
   end
