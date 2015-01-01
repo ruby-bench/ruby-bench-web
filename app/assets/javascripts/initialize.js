c208ddede1f6808ab77d817b4d23dec52ff8ad89
Guo Xiang Tan <tgx_world@hotmail.com>
2015-01-02 02:51:04 +0800

    Ensure that previous AJAX request is aborted.

--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -6,30 +6,32 @@ $(document).ready(function() {
   }
 
   var $loading = $("#loading");
-
-  $(document)
-    .ajaxStart(function() {
-      $loading.toggleClass('hide');
-    })
-    .ajaxStop(function() {
-      $loading.toggleClass('hide');
-    });
+  var xhr;
 
   $(".result-types-form input[type=radio]").change(function(value) {
+    // http://stackoverflow.com/questions/4551175/how-to-cancel-abort-jquery-ajax-request
+    if(xhr && xhr.readyState != 4){
+      xhr.abort();
+    }
+
     var $resultTypesForm = $(".result-types-form");
 
     var organizationName = $resultTypesForm.data('organization-name');
     var repoName = $resultTypesForm.data('repo-name');
     var name = $resultTypesForm.data('name')
 
-    $.ajax({
+    xhr = $.ajax({
       url: "/" + organizationName + "/" + repoName + "/" + name,
       type: 'GET',
       data: { result_type: $(this).val() },
       dataType: 'script',
       beforeSend: function() {
+        $loading.toggleClass('hide');
         $("#chart-container").empty();
         $('html,body').animate({scrollTop:0},0);
+      },
+      complete: function() {
+        $loading.toggleClass('hide');
       }
     });
   })
