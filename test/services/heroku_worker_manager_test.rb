27287ef9f9ab228fb06dbfb96da59fa1c6d2f791
Guo Xiang Tan <tgx_world@hotmail.com>
2015-01-07 17:27:21 +0800

    Don't use VCR since our API key will get exposed in the cassettes.

--- test/services/heroku_worker_manager_test.rb
+++ test/services/heroku_worker_manager_test.rb
@@ -2,15 +2,18 @@ require 'test_helper'
 
 class HerokuWorkerManagerTest < ActiveSupport::TestCase
   test "#set_worker" do
-    VCR.use_cassette("#{self.class.name.underscore}/set_worker") do
-      heroku_worker_manager = HerokuWorkerManager.new
-      client = heroku_worker_manager.client
+    PlatformAPI.expects(:connect).returns(mock('client'))
+    heroku_worker_manager = HerokuWorkerManager.new
+    client = heroku_worker_manager.client
 
-      heroku_worker_manager.set_worker(1)
-      assert_equal(client.formation.info('rubybench', 'worker')["quantity"], 1)
+    formation = mock('formation')
+    client.expects(:formation).returns(formation)
+    formation.expects(:update).with('rubybench', 'worker', { "quantity" => "1" })
+    heroku_worker_manager.set_worker(1)
 
-      heroku_worker_manager.set_worker(0)
-      assert_equal(client.formation.info('rubybench', 'worker')["quantity"], 0)
-    end
+    formation = mock('formation')
+    client.expects(:formation).returns(formation)
+    formation.expects(:update).with('rubybench', 'worker', { "quantity" => "0" })
+    heroku_worker_manager.set_worker(0)
   end
 end
