86f9bca0d1c163c20fd65a3cb0ed5794c2745119
Guo Xiang Tan <tgx_world@hotmail.com>
2015-03-08 02:05:48 +0800

    Create ChartBuilder#data upon initialization.

--- app/services/chart_builder.rb
+++ app/services/chart_builder.rb
@@ -1,34 +1,33 @@
 class ChartBuilder
   def initialize(benchmark_runs)
     @benchmark_runs = benchmark_runs
+    @data = {}
+    @data[:categories] = []
+    @data[:columns] = {}
   end
 
   def build_columns
     if !@benchmark_runs.empty?
-      data ||= {}
-      data[:categories] ||= []
-      data[:columns] ||= {}
-
       @benchmark_runs.each do |benchmark_run|
         if block_given?
-           data[:categories] << yield(benchmark_run)
+          @data[:categories] << yield(benchmark_run)
         end
 
         benchmark_run.result.each do |key, value|
-          data[:columns][key] ||= []
-          data[:columns][key] << value.to_f
+          @data[:columns][key] ||= []
+          @data[:columns][key] << value.to_f
         end
       end
 
       new_columns = []
       visible = true
-      data[:columns].each do |name, data|
+      @data[:columns].each do |name, data|
         new_columns << { name: name, data: data, visible: visible }
         visible = false
       end
-      data[:columns] = new_columns.to_json
+      @data[:columns] = new_columns.to_json
 
-      data
+      @data
     end
   end
 end
