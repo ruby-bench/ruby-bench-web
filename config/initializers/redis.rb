e4197a49572576176c2a4df46af824ad9c9cf058
Guo Xiang Tan <tgx_world@hotmail.com>
2015-04-08 23:58:04 +0800

    Update Redis config.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,10 +1,2 @@
-$redis =
-  if ENV["REDISCLOUD_URL"]
-    Redis.new(url: ENV["REDISCLOUD_URL"])
-  else
-    Redis.new(port: 6379)
-  end
-
-redis_url = ENV["REDISCLOUD_URL"] || $redis.client.id
-
-RubyBenchWeb::Application.config.cache_store = :redis_store, redis_url
+$redis = Redis.new(port: 6379)
+RubyBenchWeb::Application.config.cache_store = :redis_store, $redis.client.id
