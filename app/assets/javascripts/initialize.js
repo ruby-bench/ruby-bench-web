ebb0db64a3056ab4d65e974063dd87d2b427eb5a
Tay Yang Shun <tay.yang.shun@gmail.com>
2015-05-16 23:02:31 +0800

    Refactor benchmarks page

--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -4,83 +4,84 @@ $(document).ready(function() {
     $(".navbar-nav a[href='" + location.pathname + "']").addClass('current');
   }
 
-  $(".result-types-form input[type=radio], #benchmark_run_display_count").change(function(value) {
-    var $spinner = $(".spinner");
+  var $resultTypesForm = $('.result-types-form');
+
+  $('.result-types-form select, #benchmark_run_display_count').change(function (event) {
+    var $spinner = $('.spinner');
     var xhr;
 
     // http://stackoverflow.com/questions/4551175/how-to-cancel-abort-jquery-ajax-request
-    if(xhr && xhr.readyState != 4){
+    if (xhr && xhr.readyState !== 4) {
       xhr.abort();
     }
 
-    var $resultTypesForm = $(".result-types-form");
-
     var organizationName = $resultTypesForm.data('organization-name');
     var repoName = $resultTypesForm.data('repo-name');
-    var name = $resultTypesForm.data('name')
-    var resultType = $(".result-types-form input[type=radio]:checked").val();
-    var benchmarkRunDisplayCount = $("#benchmark_run_display_count").val();
+    var name = $resultTypesForm.data('name');
+    
+    var resultType = $('.result-types-form select').val();
+    var benchmarkRunDisplayCount = $('#benchmark_run_display_count').val();
 
-    if(benchmarkRunDisplayCount != undefined) {
+    if (benchmarkRunDisplayCount !== undefined) {
       displayCount = benchmarkRunDisplayCount;
-      displayUrlParams = "&display_count=" + benchmarkRunDisplayCount;
+      displayUrlParams = '&display_count=' + benchmarkRunDisplayCount;
     } else {
       displayCount = undefined;
       displayUrlParams = '';
     }
 
     xhr = $.ajax({
-      url: "/" + organizationName + "/" + repoName + "/" + name,
+      url: ['', organizationName, repoName, name].join('/'),
       type: 'GET',
-      data: { result_type: resultType, display_count: benchmarkRunDisplayCount },
+      data: { 
+        result_type: resultType, 
+        display_count: benchmarkRunDisplayCount 
+      },
       dataType: 'script',
-      beforeSend: function() {
+      beforeSend: function () {
         $spinner.toggleClass('hide');
-        $("#chart-container").empty();
-        $('html,body').animate({scrollTop:0},0);
+        $('#chart-container').empty();
+        $('html, body').animate({scrollTop: 0}, 0);
 
         if (history && history.pushState) {
-          history.pushState(null, '',
-            "/" +
-            organizationName +
-            "/" + repoName +
-            "/" + name +
-            '?result_type=' +
-            resultType + displayUrlParams
-          );
+          var newUrl =  '/' + organizationName +
+                        '/' + repoName +
+                        '/' + name +
+                        '?result_type=' + resultType + displayUrlParams;
+          history.pushState(null, '', newUrl);
         }
       },
-      complete: function() {
+      complete: function () {
         $spinner.toggleClass('hide');
       }
     });
-  })
+  });
 
-  drawReleaseChart(".release-chart");
-  drawChart(".chart");
+  drawReleaseChart('.release-chart');
+  drawChart('.chart');
 
   // Toggles benchmark types panel.
-  $('#benchmark-types-form-hide').click(function(e) {
+  $('#benchmark-types-form-hide').click(function (e) {
     e.preventDefault();
-    $("#benchmark-types-form-container").removeClass().toggleClass('col-xs-0');
-    $("#charts-container").removeClass().toggleClass('col-xs-12');
-    $("#benchmark-types-form-container .panel").toggleClass('hide');
-    $("#benchmark-types-form-show").toggleClass('hide');
+    $('#benchmark-types-form-container').removeClass().toggleClass('col-xs-0');
+    $('#charts-container').removeClass().toggleClass('col-xs-12');
+    $('#benchmark-types-form-container .panel').toggleClass('hide');
+    $('#benchmark-types-form-show').toggleClass('hide');
+
+    // FIXME: Figure out a way to determine which chart we have to draw
+    drawReleaseChart('.release-chart');
+    drawChart('.chart');
+  });
+
+  $('#benchmark-types-form-show').click(function (e) {
+    e.preventDefault();
+    $('#benchmark-types-form-container').removeClass().toggleClass('col-xs-3');
+    $('#charts-container').removeClass().toggleClass('col-xs-9');
+    $('#benchmark-types-form-container .panel').toggleClass('hide');
+    $('#benchmark-types-form-show').toggleClass('hide');
 
     // FIXME: Figure out a way to determine which chart we have to draw
     drawReleaseChart(".release-chart");
     drawChart(".chart");
-  })
-
-  $('#benchmark-types-form-show').click(function(e) {
-    e.preventDefault();
-    $("#benchmark-types-form-container").removeClass().toggleClass('col-xs-4');
-    $("#charts-container").removeClass().toggleClass('col-xs-8');
-    $("#benchmark-types-form-container .panel").toggleClass('hide');
-    $("#benchmark-types-form-show").toggleClass('hide');
-
-    // FIXME: Figure out a way to determine which chart we have to draw
-    drawReleaseChart(".release-chart");
-    drawChart(".chart");
-  })
+  });
 });
