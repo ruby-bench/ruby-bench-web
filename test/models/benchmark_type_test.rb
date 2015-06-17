447d81ec6b3998841dbc82082969f8844de58f69
Guo Xiang Tan <tgx_world@hotmail.com>
2015-06-17 21:32:24 +0800

    Fix pending test.

--- test/models/benchmark_type_test.rb
+++ test/models/benchmark_type_test.rb
@@ -2,6 +2,13 @@ require 'test_helper'
 
 class BenchmarkTypeTest < ActiveSupport::TestCase
   test "#latest_benchmark_run" do
-    skip('Once I find the time to transition to FactoryGirl')
+    benchmark_run = create(:release_benchmark_run)
+    benchmark_run2 = create(:release_benchmark_run, created_at: Time.zone.now - 1.day)
+
+    benchmark_type = create(:benchmark_type, benchmark_runs: [
+      benchmark_run, benchmark_run2
+    ])
+
+    assert_equal benchmark_run, benchmark_type.latest_benchmark_run('Release')
   end
 end
