95a130bb7a9c72e7f68f5036f6e686f1ece119e7
Mingding <hanmd82@gmail.com>
2015-10-04 00:16:49 +0800

    Fix outdated db seed data which was not reflecting updated schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,14 +19,20 @@ ruby_repo = Repo.create!(
 
 ao_bench = ruby_repo.benchmark_types.create!(
   category: 'ao_bench',
-  unit: 'seconds',
   script_url: 'https://raw.githubusercontent.com/ruby/ruby/trunk/benchmark/bm_app_answer.rb'
 )
 
 ab_bench = ruby_repo.benchmark_types.create!(
   category: 'ab_bench',
-  unit: 'seconds',
   script_url: 'https://raw.githubusercontent.com/ruby/ruby/trunk/benchmark/bm_app_answer.rb'
+)
+
+ao_benchmark_result_type = BenchmarkResultType.create!(
+  name: 'Execution time', unit: 'Seconds'
+)
+
+ab_benchmark_result_type = BenchmarkResultType.create!(
+  name: 'Response time', unit: 'Millieseconds'
 )
 
 10.times do |n|
@@ -42,7 +48,8 @@ ab_bench = ruby_repo.benchmark_types.create!(
     environment: 'ruby 2.2.0dev',
     initiator_id: commit.id,
     initiator_type: 'Commit',
-    benchmark_type_id: ao_bench.id
+    benchmark_type_id: ao_bench.id,
+    benchmark_result_type_id: ao_benchmark_result_type.id
   )
 
   BenchmarkRun.create!(
@@ -50,7 +57,8 @@ ab_bench = ruby_repo.benchmark_types.create!(
     environment: 'ruby 2.2.0dev',
     initiator_id: commit.id,
     initiator_type: 'Commit',
-    benchmark_type_id: ab_bench.id
+    benchmark_type_id: ab_bench.id,
+    benchmark_result_type_id: ab_benchmark_result_type.id
   )
 end
 
@@ -62,7 +70,8 @@ end
     environment: 'ruby 2.2.0dev',
     initiator_id: release.id,
     initiator_type: 'Release',
-    benchmark_type_id: ab_bench.id
+    benchmark_type_id: ab_bench.id,
+    benchmark_result_type_id: ab_benchmark_result_type.id
   )
 
   BenchmarkRun.create!(
@@ -70,6 +79,7 @@ end
     environment: 'ruby 2.2.0dev',
     initiator_id: release.id,
     initiator_type: 'Release',
-    benchmark_type_id: ao_bench.id
+    benchmark_type_id: ao_bench.id,
+    benchmark_result_type_id: ao_benchmark_result_type.id
   )
 end
