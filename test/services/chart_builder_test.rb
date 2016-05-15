86f5df11d7bfc1239ec92cf3f2b6b696f4211957
Guo Xiang Tan <tgx_world@hotmail.com>
2016-05-15 23:53:48 +0800

    Fix tests.

--- test/services/chart_builder_test.rb
+++ test/services/chart_builder_test.rb
@@ -12,7 +12,7 @@ class ChartBuilderTest < ActiveSupport::TestCase
 
     assert_equal(
       {
-        categories: ["Commit: #{benchmark_run.initiator.sha1}", "Commit: #{other_benchmark_run.initiator.sha1}"],
+        categories: ["Commit: #{benchmark_run.initiator.sha1}", "Commit: #{other_benchmark_run.initiator.sha1}"].to_json,
         columns: [
           { name: "some_time", data: [benchmark_run.result['some_time'].to_f, other_benchmark_run.result['some_time'].to_f], visible: true },
           { name: "some_other_time", data: [benchmark_run.result['some_other_time'].to_f, other_benchmark_run.result['some_other_time'].to_f], visible: false }
