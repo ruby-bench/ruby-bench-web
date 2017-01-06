63b6d1d5fc72c5dae3178ea124f18c20cb1b1492
Guo Xiang Tan <tgx_world@hotmail.com>
2017-01-06 14:01:26 +0800

    FIX: Left/Right arrow keys were trigerring stuff twice.

--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -1,10 +1,48 @@
-$(document).on('ready turbolinks:load', function() {
+$(document).on('turbolinks:load', function() {
   if (location.pathname) {
     $(".navbar-nav a[href='" + location.pathname + "']").addClass('current');
   }
 
   var $resultTypesForm = $('.result-types-form');
   var xhr;
+
+  //Add left and right arrow keys to navigate benchmark types
+  var $options = $('.result-types-form select.form-control:first option');
+  var optsLength = $options.length;
+  var $select = $('.result-types-form select.form-control:first');
+  var optionIndex = 0;
+
+  function incrementOptionIndex(index, n, increment) {
+    //calculated k because of the JavaScript modulo bug
+    k = (((index + increment) % n) + n) % n;
+    if (k == 0) {
+      k = (((k + increment) % n) + n) % n;
+    }
+    index = k;
+    return index;
+  };
+
+  $(document).keyup(function(e) {
+    $select.children().removeAttr('selected');
+
+    switch(e.which) {
+      case 37:  //left
+        optionIndex = incrementOptionIndex(optionIndex, optsLength, -1);
+        break;
+
+      case 39:  //right
+        optionIndex = incrementOptionIndex(optionIndex, optsLength, 1);
+        break;
+
+      default:
+        return;  //exit this handler for other keys
+    }
+
+    $select.children().eq(optionIndex).attr('selected', 'selected');
+    $select.val($select.children().eq(optionIndex).val())
+    $select.change();
+    e.preventDefault();  //prevent the default action (scroll / move caret)
+  });
 
   $('.result-types-form select').change(function (event) {
     var $spinner = $('.spinner');
