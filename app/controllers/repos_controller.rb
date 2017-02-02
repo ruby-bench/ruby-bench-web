f9013008b066a92ddc337256f1d785828faa9acc
Jerry Liu <jerry.liu@shopify.com>
2017-02-02 10:52:22 -0500

    Make the returned JSON object more human readable
    by putting the datapoint next to the version

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -168,23 +168,36 @@ class ReposController < ApplicationController
       result_data = chart[0]
       result_type = chart[1]
 
-      datapoints = []
+      datasets = []
       variations = []
       # each column contains an array of datapoints
       JSON.parse(result_data[:columns]).each do |column| 
         # get one set of datapoints (sometimes there's 2+ sets of data for one chart)
-        datapoints << column['data']
+        datasets << column['data']
         # This is for when there are two data sets in one chart (ex. rails commits benchmarks)
         # Example variations: `with_prepared_statements`, `without_prepared_statements`
         # `column['name']` is the benchmark name when there is only one set of datapoints
         variations << column['name']
       end
 
+      # zip the multiple datapoints together
+      first, *rest = *datasets
+      datasets_zip = first.zip(*rest)
+
+      # combine the datapoints with their respective versions
+      # `datasets_zip` and `versions` have the same length
+      datapoints = datasets_zip.zip(versions).reduce([]) do |memo, (points, version)|
+        memo << {
+          values: points,
+          "#{@repo.name}_version".to_sym => version
+        }
+        memo
+      end
+
       # generate the json
       config = {
         benchmark_name: params[:result_type],
         datapoints: datapoints,
-        "#{@repo.name}_versions".to_sym => versions,
         measurement: result_type[:name],
         unit: result_type[:unit]
       }
