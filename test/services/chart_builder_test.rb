6c6d8dfadcec211b8a795a7156fbbab7f8639dca
Jerry Liu <jerry.liu@shopify.com>
2017-02-22 11:45:33 -0500

    Fix chart tests

--- test/services/chart_builder_test.rb
+++ test/services/chart_builder_test.rb
@@ -4,21 +4,22 @@ class ChartBuilderTest < ActiveSupport::TestCase
   test "#build_columns" do
     benchmark_run = create(:commit_benchmark_run, result: { 'some_time' => 5, 'some_other_time' => 5 })
     other_benchmark_run = create(:commit_benchmark_run, result: { 'some_time' => 5, 'some_other_time' => 5 })
-    chart_builder = ChartBuilder.new([benchmark_run, other_benchmark_run])
+    chart_builder = ChartBuilder.new([benchmark_run, other_benchmark_run], benchmark_run.benchmark_result_type)
 
-    chart_columns = chart_builder.build_columns do |benchmark_run|
-      "Commit: #{benchmark_run.initiator.sha1}"
+    chart_data = chart_builder.build_columns do |benchmark_run|
+      { commit: benchmark_run.initiator.sha1 }
     end
 
     assert_equal(
-      {
-        categories: ["Commit: #{benchmark_run.initiator.sha1}", "Commit: #{other_benchmark_run.initiator.sha1}"].to_json,
-        columns: [
-          { name: "some_time", data: [benchmark_run.result['some_time'].to_f, other_benchmark_run.result['some_time'].to_f] },
-          { name: "some_other_time", data: [benchmark_run.result['some_other_time'].to_f, other_benchmark_run.result['some_other_time'].to_f] }
-        ].to_json
-      },
-      chart_columns
+      ["Commit: #{benchmark_run.initiator.sha1}", "Commit: #{other_benchmark_run.initiator.sha1}"].to_json,
+      chart_data[:categories]
+    )
+    assert_equal(
+      [
+        { name: "some_time", data: [benchmark_run.result['some_time'].to_f, other_benchmark_run.result['some_time'].to_f] },
+        { name: "some_other_time", data: [benchmark_run.result['some_other_time'].to_f, other_benchmark_run.result['some_other_time'].to_f] }
+      ].to_json,
+      chart_data[:columns]
     )
   end
 end
