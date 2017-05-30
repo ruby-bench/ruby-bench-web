3d5655ef30e842b23994212a43de3685c7157aa8
bmarkons <mamaveb@gmail.com>
2017-05-30 12:46:59 +0000

    Fix flaky test

--- test/acceptance/sponsors_test.rb
+++ test/acceptance/sponsors_test.rb
@@ -3,7 +3,10 @@ require 'acceptance/test_helper'
 class SponsorsTest < AcceptanceTest
   test "should display same number of sponsors" do
     visit root_path
-    click_link "Sponsors", match: :first
+
+    within "nav" do
+      click_link "Sponsors"
+    end
 
     assert page.has_content?(I18n.t("static_pages.sponsors.title"))
     assert page.has_css?(".row .sponsor-row", count: SponsorsData.count)
