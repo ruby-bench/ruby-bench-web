c762a8a799e58085c61ec8fa554d5452cfda46f2
bmarkons <mamaveb@gmail.com>
2017-06-09 10:59:00 +0000

    Run paginated commits when run with ManualRunner and add api_token in secrets

--- app/services/manual_runner.rb
+++ app/services/manual_runner.rb
@@ -2,20 +2,28 @@ class ManualRunner
   def initialize(repo)
     raise "Repo doesn't exist" unless Repo.exist?(repo)
     @repo = repo
+    @octokit = Octokit::Client.new(access_token: Rails.application.secrets.github_api_token)
   end
 
-  def run_commits_since(date)
-    fetched_commits = fetch_commits_since(date)
-    formatted_commits = format_commits(fetched_commits)
-
-    CommitsRunner.run(formatted_commits)
+  def run_last(commits_count)
+    run_paginated(commits_count)
   end
 
   private
 
-  def fetch_commits_since(date)
-    Octokit.auto_paginate = true
-    Octokit.commits_since("#{repo.organization.name}/#{repo.name}", since_date)
+  def run_paginated(commits_count, page = 1)
+    unless (commits_count <= 0)
+      count_run = run_commits(page)
+      run_paginated(commits_count - count_run, page + 1)
+    end
+  end
+
+  def run_commits(page)
+    fetched_commits = @octokit.commits("#{repo.organization.name}/#{repo.name}", per_page: 100, page: page)
+    formatted_commits = format_commits(fetched_commits)
+    CommitsRunner.run(formatted_commits)
+
+    fetched_commits.count
   end
 
   def format_commits(commits)
