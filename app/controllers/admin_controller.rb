d5e9f7d4096c63cc51be289086303c0f0142aa2e
bmarkons <mamaveb@gmail.com>
2017-07-06 13:08:49 +0000

    Add admin acceptance test

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,10 +4,10 @@ class AdminController < ApplicationController
     password: Rails.application.secrets.admin_password
   ) unless Rails.env.test?
 
-  layout "admin"
+  layout 'admin'
 
   before_action :set_repos
-  before_action :set_repo, :only => [:repo]
+  before_action :set_repo, only: [:repo]
 
   def toggle_admin
     session['admin'] = !session['admin']
