34fb4ec1a1cc29818b60fa1bad27683e5c762c1b
bmarkons <mamaveb@gmail.com>
2017-07-21 10:30:29 +0000

    Make index view for groups

--- app/helpers/admin/groups_helper.rb
+++ app/helpers/admin/groups_helper.rb
@@ -1,2 +1,58 @@
 module Admin::GroupsHelper
+  def group_panel_html(group)
+    "
+    <div class='col-lg-6'>
+      <div class='panel panel-default'>
+        <div class='panel-heading'>
+          #{group.name}
+        </div>
+        <div class='panel-body'>
+          <p><i>#{description_for(group)}</i></p>
+          <ul>
+            #{benchmarks_from(group)}
+          </ul>
+        </div>
+        <div class='panel-footer'>
+          #{edit_link(group)}
+          #{destroy_link(group)}
+        </div>
+      </div>
+    </div>
+    "
+  end
+
+  private
+
+  def description_for(group)
+    if group.description.present?
+      group.description
+    else
+      'No description provided'
+    end
+  end
+
+  def benchmark_types_from(group)
+    benchmark_list = ''
+    group.benchmark_types.each do |benchmark_type|
+      benchmark_list << "<li>#{benchmark_type.category}</li>\n"
+    end
+
+    benchmark_list
+  end
+
+  def edit_link(group)
+    link_to(edit_admin_group(group), class: 'btn btn-default btn-circle') do
+      '<i class="fa fa-pencil"></i>'
+    end
+  end
+
+  def destroy_link
+    link_to(destroy_admin_group(group), class: 'btn btn-danger btn-circle') do
+      '<i class="fa fa-times"></i>'
+    end
+  end
+
+  def destroy_link(group)
+
+  end
 end
