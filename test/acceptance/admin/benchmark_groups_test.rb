6bf3d2532872b63fae83aac9b1ce0410aa5f67cd
bmarkons <mamaveb@gmail.com>
2017-07-24 12:56:48 +0000

    Admin can update existing groups

--- test/acceptance/admin/benchmark_groups_test.rb
+++ test/acceptance/admin/benchmark_groups_test.rb
@@ -31,8 +31,36 @@ class BenchmarkGroupsTest < AcceptanceTest
     assert page.has_content?("#{group_name} group was successfully created.")
   end
 
-  test 'Admin should be able to update benchmark groups' do
+  test 'Admin should be able to update benchmark group' do
+    benchmark_types = create_list(:benchmark_type, 5)
+    group = create(:group)
+    group.benchmark_types << benchmark_types.first(3)
 
+    visit edit_admin_group_path(group)
+
+    assert_equal page.find('#group_name').value, group.name
+    assert_equal page.find('#group_description').value, group.description
+    group.benchmark_types.each do |benchmark_type|
+      assert page.has_checked_field?("group_benchmark_type_ids_#{benchmark_type.id}")
+    end
+
+    group_name = 'New awesome group name'
+    group_description = 'This is not chat group'
+    fill_in('Name', with: group_name)
+    fill_in('Description', with: group_description)
+    group.benchmark_types.each do |benchmark_type|
+      uncheck(benchmark_type.category)
+    end
+    benchmark_types.last(2).each do |benchmark_type|
+      check(benchmark_type.category)
+    end
+
+    click_on('Update Group')
+
+    group = Group.find(group.id)
+    assert_equal group.name, group_name
+    assert_equal group.description, group_description
+    assert_equal group.benchmark_types.to_a, benchmark_types.last(2)
   end
 
   test 'Admin should be able to destroy benchmark groups' do
