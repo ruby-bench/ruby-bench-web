04d21622e5cad7a97f3531ac46fda145c6adf8d2
bmarkons <mamaveb@gmail.com>
2017-09-11 00:02:30 +0200

    Support enqueuing releases

--- app/services/benchmark_pool.rb
+++ app/services/benchmark_pool.rb
@@ -1,17 +1,17 @@
 module BenchmarkPool
-  def self.enqueue(repo_name, commit_sha, options = {})
+  def self.enqueue(initiator_type, initiator, repo_name, options = {})
     case repo_name
     when 'ruby'
-      RemoteServerJob.perform_later(commit_sha, 'ruby_trunk', options)
-      # RemoteServerJob.perform_later(commit_sha, 'ruby_trunk_discourse')
+      RemoteServerJob.perform_later(initiator, "ruby_#{initiator_type}", options)
     when 'rails'
-      RemoteServerJob.perform_later(commit_sha, 'rails_trunk', options)
+      RemoteServerJob.perform_later(initiator, "rails_#{initiator_type}", options)
     when 'sequel'
-      RemoteServerJob.perform_later(commit_sha, 'sequel_trunk', options)
+      RemoteServerJob.perform_later(initiator, "sequel_#{initiator_type}", options)
     when 'ruby-pg'
-      RemoteServerJob.perform_later(commit_sha, 'pg_master', options)
+      RemoteServerJob.perform_later(initiator, "pg_#{initiator_type}", options)
     else
       raise ArgumentError, "unknown repo: #{repo_name}"
     end
   end
+
 end
--- test/services/benchmark_pool_test.rb
+++ test/services/benchmark_pool_test.rb
@@ -5,33 +5,49 @@ class BenchmarkPoolTest < ActiveSupport::TestCase
 
   test '#enqueue for ruby' do
     assert_enqueued_with(job: RemoteServerJob) do
-      BenchmarkPool.enqueue('ruby', 'abc')
+      BenchmarkPool.enqueue(:commit, 'abc', 'ruby')
     end
 
-    assert_enqueued_jobs 1
+    assert_enqueued_with(job: RemoteServerJob) do
+      BenchmarkPool.enqueue(:release, 'abc', 'ruby')
+    end
+
+    assert_enqueued_jobs 2 
   end
 
   test '#enqueue for rails' do
     assert_enqueued_with(job: RemoteServerJob) do
-      BenchmarkPool.enqueue('rails', 'abc')
+      BenchmarkPool.enqueue(:commit, 'abc', 'rails')
     end
 
-    assert_enqueued_jobs 1
+    assert_enqueued_with(job: RemoteServerJob) do
+      BenchmarkPool.enqueue(:release, 'abc', 'rails')
+    end
+
+    assert_enqueued_jobs 2 
   end
 
   test '#enqueue for sequel' do
     assert_enqueued_with(job: RemoteServerJob) do
-      BenchmarkPool.enqueue('sequel', 'abc')
+      BenchmarkPool.enqueue(:commit, 'abc', 'sequel')
+    end
+    
+    assert_enqueued_with(job: RemoteServerJob) do
+      BenchmarkPool.enqueue(:release, 'abc', 'sequel')
     end
 
-    assert_enqueued_jobs 1
+    assert_enqueued_jobs 2
   end
 
   test '#enqueue for ruby-pg' do
     assert_enqueued_with(job: RemoteServerJob) do
-      BenchmarkPool.enqueue('ruby-pg', 'abc')
+      BenchmarkPool.enqueue(:commit, 'abc', 'ruby-pg')
     end
 
-    assert_enqueued_jobs 1
+    assert_enqueued_with(job: RemoteServerJob) do
+      BenchmarkPool.enqueue(:release, 'abc', 'ruby-pg')
+    end
+    
+    assert_enqueued_jobs 2 
   end
 end
