35d57d3c2e858849b317690e95ebd2d95cb1bb79
Takashi Kokubun <takashikkbn@gmail.com>
2018-06-27 00:30:18 +0900

    Fix chart builder logic for JIT

--- test/services/chart_builder_test.rb
+++ test/services/chart_builder_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class ChartBuilderTest < ActiveSupport::TestCase
   test '#build_columns' do
     benchmark_run = create(:commit_benchmark_run, result: { 'some_time' => 5, 'some_other_time' => 5 })
-    other_benchmark_run = create(:commit_benchmark_run, result: { 'some_time' => 5, 'some_other_time' => 5 })
+    other_benchmark_run = create(:commit_benchmark_run, result: { 'some_time' => 5, 'some_other_time' => 5, 'jit_time' => 5 })
     chart_builder = ChartBuilder.new([benchmark_run, other_benchmark_run], benchmark_run.benchmark_result_type)
 
     chart_builder2 = chart_builder.build_columns do |benchmark_run|
@@ -19,7 +19,8 @@ class ChartBuilderTest < ActiveSupport::TestCase
     assert_equal(
       [
         { name: 'some_time', data: [benchmark_run.result['some_time'].to_f, other_benchmark_run.result['some_time'].to_f] },
-        { name: 'some_other_time', data: [benchmark_run.result['some_other_time'].to_f, other_benchmark_run.result['some_other_time'].to_f] }
+        { name: 'some_other_time', data: [benchmark_run.result['some_other_time'].to_f, other_benchmark_run.result['some_other_time'].to_f] },
+        { name: 'jit_time', data: [nil, other_benchmark_run.result['jit_time'].to_f] },
       ],
       chart_builder.columns
     )
