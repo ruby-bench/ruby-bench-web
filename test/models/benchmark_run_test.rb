e66594d3debb15a542c74bb2735cdf5b67663cfc
Osama Sayegh <asooomaasoooma90@gmail.com>
2019-05-23 16:17:37 +0300

    Release benchmarks should also filter by repo_id so we don't get mixed results from different repos (#263)
    * Release benchmarks should also filter by repo_id so we don't get mixed results
    
    * Fix rubocop

--- test/models/benchmark_run_test.rb
+++ test/models/benchmark_run_test.rb
@@ -40,4 +40,36 @@ class BenchmarkRunTest < ActiveSupport::TestCase
       BenchmarkRun.latest_commit_benchmark_run(benchmark_type, benchmark_result_type)
     )
   end
+
+  test '.fetch_release_benchmark_runs' do
+    script_url = 'https://raw.githubusercontent.com/org/repo/master/script/bench.rb'
+
+    ruby = create(:repo, name: 'ruby')
+    rails = create(:repo, name: 'rails')
+
+    ruby_release = create(:release, repo: ruby, version: '2.6.2')
+    rails_release = create(:release, repo: rails, version: '6.0.0')
+
+    ruby_benchmark_type = create(:benchmark_type, category: 'discourse_home', repo: ruby, script_url: script_url)
+    rails_benchmark_type = create(:benchmark_type, category: 'discourse_home', repo: rails, script_url: script_url)
+
+    benchmark_result_type = create(:benchmark_result_type)
+    ruby_benchmark_run = create(:release_benchmark_run,
+      benchmark_result_type: benchmark_result_type,
+      benchmark_type: ruby_benchmark_type,
+      initiator: ruby_release
+    )
+    rails_benchmark_run = create(:release_benchmark_run,
+      benchmark_result_type: benchmark_result_type,
+      benchmark_type: rails_benchmark_type,
+      initiator: rails_release
+    )
+    ruby_result = BenchmarkRun.fetch_release_benchmark_runs(ruby_benchmark_type, benchmark_result_type)
+    assert_equal(1, ruby_result.size)
+    assert_equal(ruby_benchmark_run.id, ruby_result.first.id)
+
+    rails_result = BenchmarkRun.fetch_release_benchmark_runs(rails_benchmark_type, benchmark_result_type)
+    assert_equal(1, rails_result.size)
+    assert_equal(rails_benchmark_run.id, rails_result.first.id)
+  end
 end
