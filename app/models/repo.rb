d13a5d05d831b0670fe130bd4d3ae0f9337e2f8b
OsamaSayegh <asooomaasoooma90@gmail.com>
2020-06-08 11:34:29 +0300

    Imrove performance

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -32,65 +32,59 @@ class Repo < ApplicationRecord
         FROM min_max_dates
       )
       SELECT
-        subquery.*
+        benchmark_result_type_id,
+        benchmark_type_id,
+        hstore_to_json(br.result) AS result,
+        category
       FROM (
-        SELECT
-          benchmark_result_type_id,
-          benchmark_type_id,
-          array_to_json(array_agg(br.id)) AS ids
-        FROM (
-          SELECT id FROM (
-            SELECT
-              ROW_NUMBER() OVER(PARTITION BY w.weekstart ORDER BY c.created_at) AS row_num,
-              id
-            FROM weeks w
-            INNER JOIN commits c
-            ON w.weekstart = date_trunc('week', c.created_at) AND c.repo_id = #{self.id}
-          ) x
-          WHERE row_num = 1
-        ) cw
-        INNER JOIN benchmark_runs br
-        ON cw.id = br.initiator_id AND br.initiator_type = 'Commit'
-        INNER JOIN benchmark_types bt
-        ON bt.id = br.benchmark_type_id AND bt.repo_id = #{self.id}
-        GROUP BY benchmark_type_id, benchmark_result_type_id
-      ) AS subquery
-      INNER JOIN benchmark_types
-      ON benchmark_types.id = subquery.benchmark_type_id
-      INNER JOIN benchmark_result_types
-      ON benchmark_result_types.id = subquery.benchmark_result_type_id
-      ORDER BY category, name
+        SELECT id, commit_date FROM (
+          SELECT
+            ROW_NUMBER() OVER(PARTITION BY w.weekstart ORDER BY c.created_at) AS row_num,
+            id,
+            created_at AS commit_date
+          FROM weeks w
+          INNER JOIN commits c
+          ON w.weekstart = date_trunc('week', c.created_at) AND c.repo_id = #{self.id}
+        ) x
+        WHERE row_num = 1
+      ) cw
+      INNER JOIN benchmark_runs br
+      ON cw.id = br.initiator_id AND br.initiator_type = 'Commit'
+      INNER JOIN benchmark_types bt
+      ON bt.id = br.benchmark_type_id AND bt.repo_id = #{self.id}
+      ORDER BY category, commit_date
     SQL
 
-    results = self.class.connection.execute(query).to_a
-    results.each do |row|
-      row['ids'] = JSON.parse(row['ids'])
+    raw_results = self.class.connection.execute(query).to_a
+    raw_results.each do |row|
+      row['result'] = JSON.parse(row['result'])
     end
 
-    types = self.benchmark_types
-                .where(id: results.map { |row| row['benchmark_type_id'] }.uniq)
-                .map { |type| [type.id, type] }.to_h
     result_types = BenchmarkResultType
-                   .where(id: results.map { |row| row['benchmark_result_type_id'] }.uniq)
+                   .where(id: raw_results.map { |row| row['benchmark_result_type_id'] }.uniq)
                    .map { |res_type| [res_type.id, res_type] }.to_h
-    all_runs = BenchmarkRun
-               .select(:id, :initiator_id, :result, :initiator_type, 'c.created_at')
-               .joins("JOIN commits c ON c.id = benchmark_runs.initiator_id AND benchmark_runs.initiator_type = 'Commit'")
-               .where(id: results.map { |row| row['ids'] }.flatten.uniq)
-               .map { |run| [run.id, run] }.to_h
 
-    results.each do |res|
-      type = types[res['benchmark_type_id']]
-      result_type = result_types[res['benchmark_result_type_id']]
-      runs = all_runs.values_at(*res['ids']).sort_by(&:created_at)
-      next if !type || !result_type || runs.size == 0
-
-      chart_builder = ChartBuilder.new(runs, result_type).build_columns
-      charts[type.category] ||= []
-      charts[type.category] << {
-        benchmark_result_type: result_type.name,
-        columns: chart_builder.columns
-      }
+    results = {}
+    raw_results.each do |res|
+      results[res['benchmark_type_id']] ||= {}
+      results[res['benchmark_type_id']]['category'] = res['category']
+      hash = results[res['benchmark_type_id']]['res_types'] ||= {}
+      arr = hash[result_types[res['benchmark_result_type_id']].name] ||= []
+      arr << res['result']
+      hash.sort_by { |k| k }
+      results[res['benchmark_type_id']]['res_types'] = hash.sort_by { |k, v| k }.to_h
+    end
+    results.values.each do |res|
+      category = res['category']
+      next unless category
+      res['res_types'].each do |name, runs|
+        chart_builder = ChartBuilder.new(runs, nil).build_columns_hash
+        charts[category] ||= []
+        charts[category] << {
+          benchmark_result_type: name,
+          columns: chart_builder.columns
+        }
+      end
     end
 
     $redis.setex("sparklines:#{self.id}", 1800, charts.to_msgpack)
